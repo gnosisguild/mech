314ca708d2ce99286b5fdad6cc4f2ab87ec40866
Jan-Felix <jan-felix.schwarz@gnosis.io>
2023-01-26 12:30:14 +0100

    cleanup

--- contracts/ClubCardERC721.sol
+++ contracts/ClubCardERC721.sol
@@ -1,7 +1,6 @@
 //SPDX-License-Identifier: LGPL-3.0
 pragma solidity ^0.8.9;
 
-import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
