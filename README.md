4d4740473b7c8028d1017a6c8f3bcfc96d9900df
Jan-Felix <jan-felix.schwarz@gnosis.io>
2023-01-27 13:56:04 +0100

    start with tests and docs

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Safe ClubCard
 
-Programmable Safe ownership: allow holders of designated NFTs to sign transactions
+Programmable Safe ownership
+
+#### Transferrable ownership
+
+- [ClubCardERC721.sol](contracts/ClubCardERC721.sol): allow the holder of a designated ERC-721 NFT to sign transactions
+
+#### Threshold ownership
+
+- [ClubCardERC20.sol](contracts/ClubCardERC20.sol): allow holders of a minimum balance of ERC-20 tokens to sign transactions
+- [ClubCardERC1155.sol](contracts/ClubCardERC1155.sol): allow holders of a minimum balance of ERC-1155 tokens to sign transactions
+
+#### Programmable ownership
+
+- [ClubCardBase.sol](contracts/ClubCardBase.sol): implement custom ownership terms by extending this abstract contract
---
+++ test/ClubCardERC721.ts
@@ -0,0 +1,102 @@
+import { expect } from "chai"
+import { ethers } from "hardhat"
+
+// We use `loadFixture` to share common setups (or fixtures) between tests.
+// Using this simplifies your tests and makes them run faster, by taking
+// advantage or Hardhat Network's snapshot functionality.
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers"
+
+describe("ClubCardERC721 contract", () => {
+  // We define a fixture to reuse the same setup in every test. We use
+  // loadFixture to run this setup once, snapshot that state, and reset Hardhat
+  // Network to that snapshot in every test.
+  async function deployTokenFixture() {
+    // Get the ContractFactory and Signers here.
+    const ClubCardERC721 = await ethers.getContractFactory("ClubCardERC721")
+    const [owner, addr1, addr2] = await ethers.getSigners()
+
+    // To deploy our contract, we just have to call Token.deploy() and await
+    // for it to be deployed(), which happens onces its transaction has been
+    // mined.
+    const hardhatToken = await ClubCardERC721.deploy()
+
+    await hardhatToken.deployed()
+
+    // Fixtures can return anything you consider useful for your tests
+    return { ClubCardERC721, hardhatToken, owner, addr1, addr2 }
+  }
+
+  describe("deployment", () => {
+    it("should set token address and ID", async () => {
+      // We use loadFixture to setup our environment, and then assert that
+      // things went well
+      const { hardhatToken, owner } = await loadFixture(deployTokenFixture)
+
+      // Expect receives a value and wraps it in an assertion object. These
+      // objects have a lot of utility methods to assert values.
+
+      // This test expects the owner variable stored in the contract to be
+      // equal to our Signer's owner.
+      expect(await hardhatToken.owner()).to.equal(owner.address)
+    })
+
+    it("Should assign the total supply of tokens to the owner", async () => {
+      const { hardhatToken, owner } = await loadFixture(deployTokenFixture)
+      const ownerBalance = await hardhatToken.balanceOf(owner.address)
+      expect(await hardhatToken.totalSupply()).to.equal(ownerBalance)
+    })
+  })
+
+  describe("Transactions", () => {
+    it("Should transfer tokens between accounts", async () => {
+      const { hardhatToken, owner, addr1, addr2 } = await loadFixture(
+        deployTokenFixture
+      )
+      // Transfer 50 tokens from owner to addr1
+      await expect(
+        hardhatToken.transfer(addr1.address, 50)
+      ).to.changeTokenBalances(hardhatToken, [owner, addr1], [-50, 50])
+
+      // Transfer 50 tokens from addr1 to addr2
+      // We use .connect(signer) to send a transaction from another account
+      await expect(
+        hardhatToken.connect(addr1).transfer(addr2.address, 50)
+      ).to.changeTokenBalances(hardhatToken, [addr1, addr2], [-50, 50])
+    })
+
+    it("should emit Transfer events", async () => {
+      const { hardhatToken, owner, addr1, addr2 } = await loadFixture(
+        deployTokenFixture
+      )
+
+      // Transfer 50 tokens from owner to addr1
+      await expect(hardhatToken.transfer(addr1.address, 50))
+        .to.emit(hardhatToken, "Transfer")
+        .withArgs(owner.address, addr1.address, 50)
+
+      // Transfer 50 tokens from addr1 to addr2
+      // We use .connect(signer) to send a transaction from another account
+      await expect(hardhatToken.connect(addr1).transfer(addr2.address, 50))
+        .to.emit(hardhatToken, "Transfer")
+        .withArgs(addr1.address, addr2.address, 50)
+    })
+
+    it("Should fail if sender doesn't have enough tokens", async () => {
+      const { hardhatToken, owner, addr1 } = await loadFixture(
+        deployTokenFixture
+      )
+      const initialOwnerBalance = await hardhatToken.balanceOf(owner.address)
+
+      // Try to send 1 token from addr1 (0 tokens) to owner (1000 tokens).
+      // `require` will evaluate false and revert the transaction.
+      await expect(
+        hardhatToken.connect(addr1).transfer(owner.address, 1)
+      ).to.be.revertedWith("Not enough tokens")
+
+      // Owner balance shouldn't have changed.
+      expect(await hardhatToken.balanceOf(owner.address)).to.equal(
+        initialOwnerBalance
+      )
+    })
+  })
+})
