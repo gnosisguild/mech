f6c61f2d31849b4dae2d3b782d7e12ecdfc94db1
Jan-Felix <jan-felix.schwarz@gnosis.io>
2023-02-03 12:28:14 +0100

    fix entrypoints

--- frontend/src/Card/index.tsx
+++ frontend/src/Card/index.tsx
@@ -1,6 +1,11 @@
 import React from "react"
+import { useParams } from "react-router-dom"
+import { calculateClubCardERC721Address } from "clubcard"
 
 const Card: React.FC = () => {
+  const { token, tokenId } = useParams()
+
+  calculateClubCardERC721Address(token, tokenId)
   return (
     <div>
       Connect your wallet and select one of your club cards to get started
