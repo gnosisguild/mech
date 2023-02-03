87479ce6abfe4af841d8883b41ad45718716b0f3
Jan-Felix <jan-felix.schwarz@gnosis.io>
2023-02-03 22:17:14 +0100

    frontend wip

--- frontend/src/Card/index.tsx
+++ frontend/src/Card/index.tsx
@@ -1,15 +1,44 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { calculateClubCardERC721Address } from "clubcard"
+import { useProvider } from "wagmi"
+import Layout from "../Layout"
+import { useErc721OwnerOf, useErc721Read } from "../generated"
+import { BigNumber } from "ethers"
 
 const Card: React.FC = () => {
+  const provider = useProvider()
+
+  const [deployed, setDeployed] = useState(false)
   const { token, tokenId } = useParams()
 
-  calculateClubCardERC721Address(token, tokenId)
+  if (!token || !tokenId) {
+    throw new Error("token and tokenId are required")
+  }
+  if (!token.startsWith("0x")) {
+    throw new Error("token must be a valid address")
+  }
+
+  const { data: tokenOwner } = useErc721OwnerOf({
+    address: token as `0x${string}`,
+    args: [BigNumber.from(tokenId)],
+  })
+
+  const cardAddress = calculateClubCardERC721Address(token, tokenId)
+
+  useEffect(() => {
+    provider.getCode(cardAddress).then((code) => setDeployed(code !== "0x"))
+  }, [provider, cardAddress])
+
   return (
-    <div>
-      Connect your wallet and select one of your club cards to get started
-    </div>
+    <Layout>
+      <h3>
+        Club Card {deployed ? "🟢" : "⚪️"} {cardAddress}
+      </h3>
+      <p>Token address: {token}</p>
+      <p>Token ID: {tokenId}</p>
+      <p>Token owner: {tokenOwner}</p>
+    </Layout>
   )
 }
 export default Card
