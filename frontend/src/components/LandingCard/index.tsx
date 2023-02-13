bab4ebeb33df855869bc6f06f9d2fe794f8717cb
samepant <sampanter@gmail.com>
2023-02-13 11:28:01 -0500

    landing page polish

--- frontend/src/components/LandingCard/index.tsx
+++ frontend/src/components/LandingCard/index.tsx
@@ -4,6 +4,7 @@ import gnoIcon from "cryptocurrency-icons/svg/color/gno.svg"
 
 import Button from "../Button"
 import classes from "./LandingCard.module.css"
+import clsx from "clsx"
 
 const LandingCard: React.FC = () => {
   const { open } = useWeb3Modal()
@@ -15,10 +16,13 @@ const LandingCard: React.FC = () => {
         operator of its Mech.
       </p>
       <div className={classes.demoCard}>
+        <img src="/dva_mech.png" alt="Mech" className={classes.mech} />
         <ul className={classes.demoInfo}>
           <li>
             <label>Mech</label>
-            <div className={classes.infoItem}>0x1F34...1543</div>
+            <div className={clsx(classes.infoItem, classes.address)}>
+              0x1F34...1543
+            </div>
           </li>
           <li>
             <label>Inventory</label>
@@ -31,7 +35,7 @@ const LandingCard: React.FC = () => {
           </li>
           <li>
             <label>Operator</label>
-            <div className={classes.infoItem}>
+            <div className={clsx(classes.infoItem, classes.operator)}>
               <img src="/milady142.jpg" alt="Milady #142" />
               <div>
                 <p>Milady</p>
