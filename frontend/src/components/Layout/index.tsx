4a7f2ef48fe7ab2f11c8315fb7efd3b117b98332
samepant <sampanter@gmail.com>
2023-02-13 12:46:11 -0500

    adds blockie

--- frontend/src/components/Layout/index.tsx
+++ frontend/src/components/Layout/index.tsx
@@ -6,6 +6,8 @@ import Button from "../Button"
 
 import classes from "./Layout.module.css"
 import { shortenAddress } from "../../utils/shortenAddress"
+import Blockie from "../Blockie"
+import clsx from "clsx"
 
 const Layout: React.FC<{ children: ReactNode }> = ({ children }) => {
   const { theme, setTheme } = useWeb3ModalTheme()
@@ -24,11 +26,26 @@ const Layout: React.FC<{ children: ReactNode }> = ({ children }) => {
       <header className={classes.header}>
         <h1 className={classes.title}>Mech</h1>
         <div className={classes.buttonGroup}>
-          <Button onClick={() => open({ route: "SelectNetwork" })} secondary>
+          <Button
+            onClick={() => open({ route: "SelectNetwork" })}
+            secondary
+            className={classes.button}
+          >
             chains
           </Button>
-          <Button onClick={open}>
-            {!address ? "Connect Wallet" : shortenAddress(address || "")}
+          <Button
+            onClick={open}
+            className={clsx(classes.button, address && classes.connectedButton)}
+            secondary={!!address}
+          >
+            {!address ? (
+              <p>Connect Wallet</p>
+            ) : (
+              <div className={classes.connectedAccount}>
+                <Blockie className={classes.blockie} address={address} />
+                <p>{shortenAddress(address || "")}</p>
+              </div>
+            )}
           </Button>
         </div>
       </header>
