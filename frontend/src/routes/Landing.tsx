a43d0b72597da33b7ad729e9cebd3c342454351f
samepant <sampanter@gmail.com>
2023-02-13 15:34:46 -0500

    route rename

--- frontend/src/routes/Landing.tsx
+++ frontend/src/routes/Landing.tsx
@@ -1,12 +1,12 @@
 import React from "react"
 import Layout from "../components/Layout"
 import LandingCard from "../components/LandingCard"
+import { useAccount } from "wagmi"
 
 const Landing: React.FC = () => {
+  const { address } = useAccount()
   return (
-    <Layout>
-      <LandingCard />
-    </Layout>
+    <Layout>{address ? <p>Connected to {address}</p> : <LandingCard />}</Layout>
   )
 }
 export default Landing
