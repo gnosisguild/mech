dab2e62f45812537bfc116810052341101f2ebc1
samepant <sampanter@gmail.com>
2023-02-15 16:54:19 -0500

    parse tokenUrl

--- frontend/src/components/NFTGrid/index.tsx
+++ frontend/src/components/NFTGrid/index.tsx
@@ -2,22 +2,25 @@ import { useNFTsByOwner } from "ankr-react"
 
 import classes from "./NFTGrid.module.css"
 import NFTItem from "../NFTItem"
+import Spinner from "../Spinner"
 
 interface Props {
   address: string
 }
 
 const NFTGrid: React.FC<Props> = ({ address }) => {
-  const { data, error, isLoading } = useNFTsByOwner({
+  const { data, isLoading } = useNFTsByOwner({
     walletAddress: "0xc6B69B579b94Cc2E89Ab37c5Dd4EDe3DF05DF04E",
     blockchain: "eth",
   })
 
   return (
     <div className={classes.container}>
+      {isLoading && <Spinner />}
       <ul className={classes.grid}>
         {data?.assets
           .filter((nft) => nft.tokenUrl)
+          .filter((nft) => nft.tokenId)
           .map((nft, index) => (
             <li key={`${index}-${nft.contractAddress}`}>
               <NFTItem nft={nft} />
--- frontend/src/components/NFTItem/index.tsx
+++ frontend/src/components/NFTItem/index.tsx
@@ -2,24 +2,70 @@ import { Nft } from "@ankr.com/ankr.js"
 
 import classes from "./NFTItem.module.css"
 import Button from "../Button"
+import { useEffect, useState } from "react"
 
 interface Props {
   nft: Nft
 }
 
+const useTokenUrl = (tokenUrl: string | undefined) => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState<any>(false)
+  const [data, setData] = useState<any>(null)
+
+  useEffect(() => {
+    if (!tokenUrl) {
+      setIsLoading(false)
+      return
+    }
+
+    const fetchData = async () => {
+      if (tokenUrl.slice(0, 4) === "http") {
+        try {
+          const response = await fetch(tokenUrl)
+          const data = await response.json()
+          setData(data)
+          setIsLoading(false)
+        } catch (e) {
+          setError(e)
+          setIsLoading(false)
+        }
+      } else if (tokenUrl.slice(0, 29) === "data:application/json;base64,") {
+        const json = Buffer.from(tokenUrl.slice(29), "base64").toString()
+        setData(JSON.parse(json))
+        setIsLoading(false)
+      }
+    }
+
+    fetchData()
+  }, [tokenUrl])
+
+  return { isLoading, data, error }
+}
+
 const NFTItem: React.FC<Props> = ({ nft }) => {
-  console.log(nft)
+  const needTokenUrl = !nft.imageUrl
+  const { isLoading, data, error } = useTokenUrl(
+    needTokenUrl ? nft.tokenUrl : undefined
+  )
   return (
     <div className={classes.itemContainer}>
       <div className={classes.header}>
-        <p className={classes.tokenName}>{nft.name || "..."}</p>
+        <p className={classes.tokenName}>
+          {nft.name || nft.collectionName || "..."}
+        </p>
         {nft.tokenId.length < 5 && (
           <p className={classes.tokenId}>{nft.tokenId || "..."}</p>
         )}
       </div>
       <div className={classes.main}>
-        {nft.imageUrl && (
-          <img src={nft.imageUrl} alt={nft.name} className={classes.image} />
+        {error && <div className={classes.noImage}></div>}
+        {!isLoading && !error && (
+          <img
+            src={data ? data.image : nft.imageUrl}
+            alt={nft.name}
+            className={classes.image}
+          />
         )}
         <div className={classes.info}></div>
       </div>
