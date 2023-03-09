6043dabf7872e2ceefd119dd25d1ac2ddb118f2c
Jan-Felix <jan-felix.schwarz@gnosis.io>
2023-03-09 16:59:46 +0100

    add comments

--- .solcover.js
+++ .solcover.js
@@ -1,3 +1,6 @@
 module.exports = {
-  skipFiles: ["libraries/SafeMigration.sol", "test"], // this is only tested with integration tests, not the standard unit tests
+  skipFiles: [
+    "libraries/SafeMigration.sol", // this is only tested with integration tests, not the standard unit tests
+    "test", // we don't care about the contracts we just use for testing purposes
+  ],
 }
