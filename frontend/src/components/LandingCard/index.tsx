9d913750fb0387f9005f69198dba5f5ff6e55668
samepant <sampanter@gmail.com>
2023-05-10 07:45:51 -0700

    add idle animation to homepage

--- frontend/src/components/LandingCard/index.tsx
+++ frontend/src/components/LandingCard/index.tsx
@@ -16,7 +16,7 @@ const LandingCard: React.FC = () => {
         operator of its Mech.
       </p>
       <div className={classes.demoCard}>
-        <img src="/mech_sprite.png" alt="Mech" className={classes.mech} />
+        <img src="/mech-idle.gif" alt="Mech" className={classes.mech} />
         <ul className={classes.demoInfo}>
           <li>
             <label>Mech</label>
