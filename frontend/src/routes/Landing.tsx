a74f6af7b8369fa9e64eb4d18c7c18dcff3ea32e
samepant <sampanter@gmail.com>
2024-01-19 20:55:52 -0500

    add guild badge to landing page

--- frontend/src/routes/Landing.tsx
+++ frontend/src/routes/Landing.tsx
@@ -2,11 +2,32 @@ import React from "react"
 import Layout from "../components/Layout"
 import LandingCard from "../components/LandingCard"
 
+import classes from "./Landing.module.css"
+
 const Landing: React.FC = () => {
   return (
-    <Layout>
-      <LandingCard />
-    </Layout>
+    <>
+      <Layout>
+        <LandingCard />
+      </Layout>
+      <footer className={classes.footer}>
+        <a
+          className={classes.gg}
+          href="https://www.gnosisguild.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="/gnosisguild.png"
+            alt="Gnosis Guild"
+            width={24}
+            height={24}
+            className={classes.logo}
+          />
+          <p>Built by Gnosis Guild</p>
+        </a>
+      </footer>
+    </>
   )
 }
 export default Landing
